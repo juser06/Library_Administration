6b39458b94acf0a3ddb955b0b0574e87e3feb66c
Jus_Notebook <Notebook>
2023-04-20 17:08:31 -0300

    emprece a escribir los commands de los botones

---
+++ Library/ViewModel/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string username;
+        private string password;
+        private string errorMessage;
+        private bool isViewVisible = true;
+    }
+}
---
+++ Library/ViewModel/ViewModelCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Library.ViewModel
+{
+    public class ViewModelCommand : ICommand
+    {
+        //fields
+        private readonly Action<object> _executeAction;
+        private readonly Predicate<object> _canExcecuteAction;
+
+        //constructor
+        public ViewModelCommand(Action<object> executeAction)
+        {
+            _executeAction = executeAction;
+        }
+        public ViewModelCommand(Action<object> executeAction, Predicate<object> canExcecuteAction)
+        {
+            _executeAction = executeAction;
+            _canExcecuteAction = canExcecuteAction;
+        }
+
+        //event
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        //methods
+        public bool CanExecute(object parameter)
+        {
+            return _canExcecuteAction == null ? true : _canExcecuteAction(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeAction(parameter);
+        }
+    }
+}
