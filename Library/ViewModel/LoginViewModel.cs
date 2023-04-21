256d788b5775dc1a2f3f161e9c27920352984bf7
Jus_Notebook <Notebook>
2023-04-21 15:03:46 -0300

    adelante un poco los command

--- Library/ViewModel/LoginViewModel.cs
+++ Library/ViewModel/LoginViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +10,60 @@ namespace Library.ViewModel
     public class LoginViewModel : ViewModelBase
     {
         private string username;
-        private string password;
+        private SecureString password;
         private string errorMessage;
         private bool isViewVisible = true;
+
+        //properties
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public SecureString Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage 
+        {
+            get
+            {
+                return errorMessage;
+            }
+            set
+            {
+                errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible 
+        {
+            get
+            {
+                return isViewVisible;
+            }
+            set
+            {
+                isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+        // -> Command
+
     }
 }
